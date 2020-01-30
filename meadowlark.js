4da790e6ef7ee11919cff2f633185b3714a8667f
Cesar Acual <chechaacual@gmail.com>
2020-01-30 11:43:53 -0600

    Static Files and Views

--- meadowlark.js
+++ meadowlark.js
@@ -11,6 +11,8 @@ app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => res.render('about'))
