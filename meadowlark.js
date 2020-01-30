87f2b859ab2fd15239e99fb2b342a2fb22222da5
Cesar Acual <chechaacual@gmail.com>
2020-01-30 12:05:08 -0600

    Dynamic Content in Views

--- meadowlark.js
+++ meadowlark.js
@@ -13,9 +13,20 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
 
 // custom 404 page
 app.use((req, res) => {
