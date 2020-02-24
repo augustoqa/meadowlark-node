614650980c600abd1140389990ff3e52b58034a9
Cesar Acual <chechaacual@gmail.com>
2020-02-24 16:20:29 -0600

    Refactoring the Application for Testability

--- meadowlark.js
+++ meadowlark.js
@@ -3,7 +3,7 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express();
 
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
@@ -15,24 +15,15 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.log(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, console.log(
     `Express started on http://localhost:${port}; press Ctrl+C to terminate.`
